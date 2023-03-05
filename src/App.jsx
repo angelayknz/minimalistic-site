b9d04d711669888d8ef03f8fbb1f558f0f7c10b7
angela <angela_yk@hotmail.com>
2023-03-05 23:28:38 +1300

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
+import './App.css'
+import Header from "./components/Header/Header"
+import Home from './components/home/Home'
+
 function App() {
 
   return (
-    <div>
-      Hello world! 
-    </div>
+    <>
+      <Header />
+      <main className="main">
+        <Home />
+      </main>
+    </>
   )
 }
 
